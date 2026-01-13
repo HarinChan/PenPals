176e25fe512ae6c1d1b1d777089fc51dea079632
HarinChan <harin.c325@gmail.com>
2026-01-13 19:17:10 +0000

    Updated requirements

---
+++ build_target.py
@@ -0,0 +1,12 @@
+import subprocess
+import time
+
+python_executable = "/penpals-backend/dist/app.exe"
+tauri_executable = "/penpals-frontend/src-tauri/target/release/bundle/nsis/penpals-frontend_0.1.0_x64-setup.exe"
+
+# Start the Python application
+python_process = subprocess.Popen([python_executable])
+# Wait for a few seconds to ensure the Python app is running
+time.sleep(1)
+# Start the Tauri application
+subprocess.run([tauri_executable])
